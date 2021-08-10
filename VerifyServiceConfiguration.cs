bf616ef5d1542dd2f2a136461bd2a7151d17740a
Roch DESCHASEAUX <roch.deschaseaux@mafassurances.onmicrosoft.com>
2021-08-10 17:35:33 +0200

    Remove logging from amazon secret vault getter

--- VerifyServiceConfiguration.cs
+++ VerifyServiceConfiguration.cs
@@ -3,28 +3,23 @@
     using Amazon.SecretsManager;
     using Amazon.SecretsManager.Model;
     using Microsoft.Extensions.Configuration;
-    using Microsoft.Extensions.Logging;
     using Newtonsoft.Json;
     using System.Threading.Tasks;
 
     public class VerifyServiceConfiguration : IVerifyServiceConfiguration
     {
         private readonly IAmazonSecretsManager _amazonSecretsManagerClient;
-        private readonly ILogger _logger;
 
         public VerifyServiceConfiguration(
             IConfiguration configuration,
             IAmazonSecretsManager amazonSecretsManagerClient,
-            IEnvironmentConfiguration environmentConfiguration,
-            ILogger<VerifyServiceConfiguration> logger)
+            IEnvironmentConfiguration environmentConfiguration)
         {
             _amazonSecretsManagerClient = amazonSecretsManagerClient;
-            _logger = logger;
             SecretKey =
                 environmentConfiguration.IsProduction ?
                 GetRecaptchaSecretKey().Result :
                 configuration["verifyServiceSecretKey"];
-            logger.LogInformation(new { environmentConfiguration.IsProduction }.ToString());
         }
 
         public string SecretKey { private set; get; }
@@ -37,13 +32,11 @@
                 VersionStage = "AWSCURRENT"
             };
             var secretValueResponse = await _amazonSecretsManagerClient.GetSecretValueAsync(request);
-            _logger.LogInformation(new { secretValueRequestId = secretValueResponse.ResponseMetadata.RequestId }.ToString());
-            var secretValueResponseMetadata = JsonConvert.SerializeObject(secretValueResponse.ResponseMetadata.Metadata);
-            _logger.LogInformation(secretValueResponseMetadata);
 
-            var recaptchaSecretKey = 
-                JsonConvert.DeserializeAnonymousType(secretValueResponse.SecretString, new { RecaptchaSecretKey = "" }).RecaptchaSecretKey;
-            return recaptchaSecretKey;
+            return
+                JsonConvert.DeserializeAnonymousType(
+                    secretValueResponse.SecretString, new { RecaptchaSecretKey = "" })
+                .RecaptchaSecretKey;
         }
     }
 }
